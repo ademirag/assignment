38fbd0f61e1cdc13d0d405fffde7823752738be7
Asım Demirağ <asimdemirag@gmail.com>
2021-01-10 13:14:44 +0300

    strapi installation

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1b5b17c500b52cfd54cbd13a0063bece'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ff0aa5f8-51da-43c0-b476-05e2aeef71e1'
+};
